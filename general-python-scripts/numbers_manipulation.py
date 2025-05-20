e39cc02bc684393c57aa0c9a7b5cc1966eda612c
Carbonard <Carbonard@users.noreply.github.com>
2025-05-21 00:22:17 +0200

    Fix little errors in numbers_manipulation.py

--- general-python-scripts/numbers_manipulation.py
+++ general-python-scripts/numbers_manipulation.py
@@ -1,25 +1,30 @@
 def first_digit(num):
-    num=abs(num)
-    if num>=1:
-        while num//10>0:
-            num //=10
-    else:
-        while num*10<1:
-            num *=10
-        num = round(num*10)
-    return num
+	num = abs(num)
+	if num >= 1:
+		while num >= 10:
+			num //= 10
+	else:
+		while num < 1:
+			num *= 10
+	return int(num)
 
-def scientific_notation(num,trunc=16):
-    sign = "-" if num < 0 else ""
-    exp=0
-    if num>=1:
-        while num/10>1:
-            num /=10
-            exp += 1
-    elif num>0:
-        while num*10<10:
-            num *=10
-            exp -=1
-        num = num
-    if trunc == 0: trunc-=1 # To delete the floating point from next string
-    return sign+str(num)[:trunc+2]+f"*10^{exp}"
+def scientific_notation(num, trunc=16):
+	sign = "-" if num < 0 else ""
+	num = abs(num)
+	exp = 0
+	if num >= 1:
+		while num >= 10:
+			num /= 10
+			exp += 1
+	elif num > 0:
+		while num < 1:
+			num *= 10
+			exp -= 1
+		num = num
+	if num == int(num): # To avoid resulst like 2.0
+		num = int(num)
+	if trunc == 0: trunc -= 1 # To delete the floating point from next string
+	result = sign + str(num)[:trunc+2]
+	if exp != 0:
+		result += f"*10^{exp}"
+	return result
